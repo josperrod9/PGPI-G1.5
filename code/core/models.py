33eae95964d81690ea3fed2bf6c59bda70fcf035
Andreu Montagut Osuna <80252401+andmonosu@users.noreply.github.com>
2022-12-08 21:15:41 +0100

    Add files via upload

--- code/core/models.py
+++ code/core/models.py
@@ -158,7 +158,7 @@ class Address(models.Model):
 class Payment(models.Model):
     stripe_charge_id = models.CharField(max_length=50)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.SET_NULL, blank=True, null=True)
+                             on_delete=models.CASCADE, blank=True, null=True)
     amount = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
