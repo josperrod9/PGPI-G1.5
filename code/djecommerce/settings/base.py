265c834ff22374d0a5be2b173a168abe1102e52b
andmonosu <andmonosu@alum.us.es>
2022-12-08 16:37:18 +0100

    Nuevo settings

--- code/djecommerce/settings/base.py
+++ code/djecommerce/settings/base.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reset_migrations',
     'django.contrib.sites',
     'allauth',
     'crispy_forms',
